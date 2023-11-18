d7c32a7d6400558b5e7f6152fa22eafa59f9e8ec
Nick Spinosa <nickspinosa1022@gmail.com>
2023-11-17 21:19:28 -0500

    wip

--- src/util/math_utils.rs
+++ src/util/math_utils.rs
@@ -1,55 +1,78 @@
-use std::collections::{HashSet, HashMap};
+use factorial::factorial;
+use std::collections::HashMap;
 
-const FACTORIAL_TABLE: HashMap<i32, i64> = HashMap::new();
-
-pub fn multiset_permute(set: Vec<i32>) {
+// [1, 1, 2, 4] multiset
+pub fn multiset_permute(set: Vec<usize>) {
     // let permutations = vec![vec![0, set.len()], calculate_unique_permutations(set)];
-}
+    let mut permutations: Vec<Vec<usize>> = Vec::with_capacity(calculate_unique_permutations(&set));
+    for v in &set {
+        //create single permutation
+        //push into permutations
+        let single_perm: Vec<usize> = Vec::with_capacity(set.len());
 
-fn calculate_unique_permutations(set: Vec<i32>) -> i32 {
-    let multiplicities = calculate_multiplicities(set);
-    let max = multiplicities.iter().max();
-}
-
-
-fn calculate_multiplicities(set: Vec<i32>) -> Vec<i32> {
-    let map: HashMap<i32, i32> = set
-        .iter()
-        .fold(HashMap::new(), |mut map, ele| {
-            map.entry(ele.to_owned())
-                .and_modify(|v| {*v += 1})
-                .or_insert(1);
-            return map;
-        });
-    
-    return map.keys().cloned().collect();
-}
-
-fn factorial(num: i32) -> i32 {
-    if let (FACTORIAL_TABLE.get(num)) {
-
+        permutations.push(single_perm);
     }
-    let mut result: i32 = 1;
-
-    for i in 2 .. num {
-        result *= i;
-    }
-    return result;
 }
 
+// n!/(m1! * m2! ... * mn!)
+fn calculate_unique_permutations(set: &Vec<usize>) -> usize {
+    let multiplicities = calculate_multiplicities(&set);
 
+    let multi_facs = multiplicities.iter().fold(1 as usize, |a, e| {
+        return a.to_owned() * factorial(e.to_owned()); 
+    });
 
-// #[test]
-// fn test_unique_elements() {
-//     let set = vec![1, 1, 2, 4];
-//     let val = unique_elements(set);
-//
-//     assert_eq!(val, 3);
-// }
+    return factorial(set.len()) / multi_facs;
+}
 
-// [1, 1, 2, 4] multiset 
+fn calculate_multiplicities(set: &Vec<usize>) -> Vec<usize> {
+    let map: HashMap<usize, usize> = set.iter().fold(HashMap::new(), |mut map, ele| {
+        map.entry(ele.to_owned())
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+        return map;
+    });
+
+    return map.values().cloned().collect();
+}
+
+mod factorial {
+    use std::collections::HashMap;
+    use once_cell::sync::Lazy;
+
+    const FACTORIAL_TABLE: Lazy<HashMap<usize, usize>> = Lazy::new(|| HashMap::new());
+
+    pub fn factorial(num: usize) -> usize {
+        if num < 2 {
+            return 1;
+        }
+        if let Some(fac) = FACTORIAL_TABLE.get(&num) {
+            return fac.to_owned();
+        }
+
+        let res = num * factorial(num - 1);
+        FACTORIAL_TABLE.insert(num, res);
+
+        return res;
+    }
+}
+
+// [1, 1, 2, 4] multiset
 // [(1, 2), (2, 1), (4, 1)]
 // n!/(m1! * m2! ... * mn!)
 // [2, 1, 1
 // [1, 1, 1, 1, 0, 0, 0, 0,]
 // 8! / (4! * 4!)
+
+#[test]
+fn test_calc_uniq_perms() {
+    let multis = vec![1, 1, 2, 4];
+
+    assert_eq!(calculate_unique_permutations(multis), 12);
+}
+
+#[test]
+fn test_fac() {
+    let fac_8 = 40320;
+    assert_eq!(factorial(8), fac_8);
+}
