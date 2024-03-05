9a847c8998f20568f0ab4a981fd6d2a4fbc0651e
John Freese <johnfreese105@gmail.com>
2024-03-04 19:05:21 -0500

    working multiset permute

--- src/util/linked_list.rs
+++ src/util/linked_list.rs
@@ -1,176 +1,106 @@
-use std::{cell::RefCell, borrow::BorrowMut};
+use std::{
+    borrow::BorrowMut,
+    cell::RefCell,
+    fmt::{format, Debug},
+    ptr::{self, null, null_mut},
+};
 
-#[derive(Debug, PartialEq, Eq)]
+// #[derive(Debug)]
 pub struct LinkedList<T> {
-    pub head: Link<T>,
+    head: Link<T>,
+    tail: *mut Node<T>,
+    pub size: usize,
 }
 
-type Link<T> = RefCell<Option<Box<Node<T>>>>;
+type Link<T> = *mut Node<T>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(PartialEq, Eq)]
 pub struct Node<T> {
     pub value: T,
     pub next: Link<T>,
 }
 
-impl<T> Node<T> {
-    pub fn new(value: T) -> Node<T> {
-        Node {
-            value,
-            next: RefCell::from(None)
-        }
-    }
-
-    pub fn newLink(value: T) -> Link<T> {
-        RefCell::from(Some(Box::new(Node::new(value))))
-    }
-
-    pub fn emptyLink() -> Link<T> {
-        RefCell::from(None)
-    }
-
-    pub fn append(&mut self, value: T) {
-        match *self.next.borrow_mut() {
-            None => {
-                self.next = Node::newLink(value);
-            }
-            Some(mut next) => next.append(value),
-        }
-    }
-}
-
-#[macro_export]
-macro_rules! ll {
-    ( $( $x:expr ),* ) => {
-        {
-            let mut temp_ll= LinkedList::new();
-            $(
-                temp_ll.append($x);
-            )*
-            temp_ll
-        }
-    };
-}
-
-impl<T> LinkedList<T> {
-    pub fn new() -> Self {
-        return LinkedList { head: Node::emptyLink() };
-    }
-
-    pub fn push(&mut self, value: T) {
-        let node = Box::new(Node {
-            value,
-            next: RefCell::from(self.head.take()),
-        });
-
-        self.head = RefCell::from(Some(node));
-    }
-
-    pub fn append(&mut self, value: T) {
-        match *self.head.borrow_mut() {
-            None => {
-                let new_node = Node::newLink(value);
-                self.head = new_node;
-            }
-            Some(mut node) => node.append(value),
-        }
-    }
-
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|n| {
-            self.head = n.next;
-            n.value
-        })
-    }
-
-    pub fn peek(&self) -> Option<&T> {
-        self.head.borrow_mut().map(|n| &n.value)
-    }
-
-    pub fn peek_mut(&mut self) -> Option<&mut T> {
-        self.head.borrow_mut().map(|mut n| &mut n.value)
-    }
-
-    pub fn get_mut(&mut self, index: usize) -> Option<&mut Node<T>> {
-        let mut node = self.iter_mut();
-        // probably need to be smarter about this
-        for _ in 0..index {
-            node.next();
-        }
-
-        return node.next;
-    }
-
-    pub fn into_iter(self) -> IntoIter<T> {
-        IntoIter(self)
-    }
-
-    pub fn iter(&self) -> Iter<'_, T> {
-        Iter {
-            next: self.head.borrow_mut().map(|n| &*n),
-        }
-    }
-
-    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
-        IterMut {
-            next: self.head.get_mut().as_deref_mut(),
-        }
-    }
-}
-
-/* Into Iter */
-
-#[derive(Debug)]
-pub struct IntoIter<T>(LinkedList<T>);
-
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop()
-    }
-}
-
-/* Immutable Iterator*/
-
-#[derive(Debug)]
 pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            head: ptr::null_mut(),
+            tail: ptr::null_mut(),
+            size: 0,
+        }
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        // self.next.map(|n| {
-        //     self.next = n.next.borrow().as_deref();
-        //     &n.value
-        // })
-        match self.next.borrow_mut() {
-            None => None,
-            Some(next) => {
-                self.next = next.next.borrow_mut().as_deref();
-                Some(&next.value)
+    pub fn push(&mut self, value: T) {
+        unsafe {
+            let new_tail = Box::into_raw(Box::new(Node {
+                value,
+                next: ptr::null_mut(),
+            }));
+
+            if !self.tail.is_null() {
+                (*self.tail).next = new_tail;
+            } else {
+                self.head = new_tail;
+            }
+
+            self.size += 1;
+            self.tail = new_tail;
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        unsafe {
+            if self.head.is_null() {
+                None
+            } else {
+                let head = Box::from_raw(self.head);
+                self.head = head.next;
+
+                if self.head.is_null() {
+                    self.tail = ptr::null_mut();
+                }
+
+                self.size -= 1;
+                Some(head.value)
             }
         }
     }
-}
 
-/* Mutable Iterator*/
+    pub fn peek(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.value) }
+    }
 
-#[derive(Debug)]
-pub struct IterMut<'a, T> {
-    next: Option<&'a mut Node<T>>,
-}
+    pub fn iter(&self) -> Iter<'_, T> {
+        unsafe {
+            Iter {
+                next: self.head.as_ref(),
+            }
+        }
+    }
 
-impl<'a, T> Iterator for IterMut<'a, T> {
-    type Item = &'a mut T;
+    pub unsafe fn set_head(&mut self, new_head: *mut Node<T>) {
+        self.head = new_head
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.take().map(|n| {
-            self.next = n.next.borrow_mut().as_deref_mut();
-            &mut n.value
-        })
+    pub unsafe fn get_raw_reference(&self, index: usize) -> *mut Node<T> {
+        if index > self.size - 1 {
+            return ptr::null_mut();
+        }
+
+        if self.head.is_null() {
+            return ptr::null_mut();
+        }
+
+        let mut self_ptr = self.head;
+
+        for i in 0..index {
+            self_ptr = (*self_ptr).next;
+        }
+
+        return self_ptr;
     }
 }
 
@@ -180,7 +110,7 @@ impl<T> From<Vec<T>> for LinkedList<T> {
         let mut ll = LinkedList::new();
 
         for val in vec {
-            ll.append(val);
+            ll.push(val);
         }
 
         return ll;
@@ -188,43 +118,122 @@ impl<T> From<Vec<T>> for LinkedList<T> {
 }
 
 /* From LinkedList */
-impl<T> From<LinkedList<T>> for Vec<T> {
-    fn from(ll: LinkedList<T>) -> Self {
-        let mut vec = Vec::new();
+impl<T: Copy> From<&LinkedList<T>> for Vec<T> {
+    fn from(ll: &LinkedList<T>) -> Self {
+        unsafe {
+            let mut vec = Vec::new();
+            let mut ll_ptr = ll.head;
+            for i in 0..ll.size {
+                if ll_ptr != null_mut() {
+                    let val = ll_ptr.as_ref().ok_or("panic!").unwrap().value;
+                    vec.push(val);
+                    ll_ptr = (*ll_ptr).next;
+                } else {
+                    break;
+                }
+            }
 
-        for val in ll.into_iter() {
-            vec.push(val);
-        } 
+            return vec;
+        }
+    }
+}
 
-        return vec;
+impl<T: PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        unsafe {
+            if self.size != other.size {
+                return false;
+            }
+
+            if self.size == 0 {
+                return true;
+            }
+
+            let mut self_ptr = self.head;
+            let mut other_ptr = other.head;
+
+            for i in 0..self.size {
+                if self_ptr != null_mut() {
+                    let self_val = &(*self_ptr).value;
+                    let other_val = &(*other_ptr).value;
+
+                    if self_val != other_val {
+                        return false;
+                    }
+
+                    self_ptr = (*self_ptr).next;
+                    other_ptr = (*other_ptr).next;
+                } else {
+                    break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
+impl<T: PartialEq> Eq for LinkedList<T> {}
+
+impl<T: Debug> Debug for LinkedList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.size == 0 {
+            write!(f, "{{ }}");
+        }
+
+        let mut output: Vec<String> = Vec::with_capacity(self.size + 2);
+        output[0] = "{".to_owned();
+        unsafe {
+            let mut self_ptr = self.head;
+            for i in 0..self.size {
+                if self_ptr != null_mut() && (*self_ptr).next != null_mut() {
+                    let val = self_ptr.as_ref().ok_or("panic!").unwrap();
+                    output[i + 1] = format!("{:?}->", val);
+                    self_ptr = (*self_ptr).next;
+                } else if self_ptr != null_mut() {
+                    let val = self_ptr.as_ref().ok_or("panic!").unwrap();
+                    output[i + 1] = format!("{:?}", val);
+                    self_ptr = (*self_ptr).next;
+                } else {
+                    break;
+                }
+            }
+        }
+        output[self.size + 1] = "}".to_owned();
+        write!(f, "{:?}", &output.concat())
+    }
+}
+
+impl<T: Debug> Debug for Node<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{ value: {:?} }}", self.value)
     }
 }
 
 /* Drop */
 impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
-        let mut cur_link = self.head.take();
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = boxed_node.next.take();
+        while let Some(_) = self.pop() {}
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.map(|node| {
+                self.next = node.next.as_ref();
+                &node.value
+            })
         }
-    }
-}
-
-/* Clone */ 
-impl<T: Clone> Clone for LinkedList<T> {
-    fn clone(&self) -> Self {
-        Self { head: self.head.clone() }
-    }
-}
-
-impl<T: Clone> Clone for Node<T> {
-    fn clone(&self) -> Self {
-        Self { value: self.value.clone(), next: self.next.clone() }
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::ll;
+
     use super::LinkedList;
 
     #[test]
@@ -240,7 +249,7 @@ mod test {
         list.push(3);
 
         // Check normal removal
-        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
 
         // Push some more just to make sure nothing's corrupted
@@ -248,131 +257,76 @@ mod test {
         list.push(5);
 
         // Check normal removal
-        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(4));
 
         // Check exhaustion
-        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(5));
         assert_eq!(list.pop(), None);
     }
 
     #[test]
-    fn two_pointers() {}
+    fn two_pointers() {
+        let actual = ll!(1, 2, 3);
+
+        unsafe {
+            let mut ptr_one = actual.get_raw_reference(0);
+            let mut ptr_two = actual.get_raw_reference(1);
+
+            let temp = (*ptr_two).next;
+            (*ptr_two).next = ptr_one;
+            (*ptr_one).next = temp;
+        }
+
+        let expected = ll!(1, 3, 2);
+
+        assert_eq!(actual, expected);
+    }
 
     #[test]
     fn peek() {
         let mut list = LinkedList::new();
         assert_eq!(list.peek(), None);
-        assert_eq!(list.peek_mut(), None);
         list.push(1);
         list.push(2);
         list.push(3);
 
-        assert_eq!(list.peek(), Some(&3));
-        assert_eq!(list.peek_mut(), Some(&mut 3));
-        list.peek_mut().map(|value| *value = 42);
-
-        assert_eq!(list.peek(), Some(&42));
-        assert_eq!(list.pop(), Some(42));
-    }
-
-    #[test]
-    fn into_iter() {
-        let mut list = LinkedList::new();
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        let mut iter = list.into_iter();
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next(), Some(2));
-        assert_eq!(iter.next(), Some(1));
-        assert_eq!(iter.next(), None);
-    }
-
-    #[test]
-    fn iter() {
-        let mut list = LinkedList::new();
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        let mut iter = list.iter();
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&1));
-    }
-
-    #[test]
-    fn iter_mut() {
-        let mut list = LinkedList::new();
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        let mut iter = list.iter_mut();
-        assert_eq!(iter.next(), Some(&mut 3));
-        assert_eq!(iter.next(), Some(&mut 2));
-        assert_eq!(iter.next(), Some(&mut 1));
-    }
-
-    #[test]
-    fn get_iter() {
-        let mut list = LinkedList::new();
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        let actual = list.get_mut(1).map(|n| n.value);
-        assert_eq!(actual, Some(2));
-    }
-
-    #[test]
-    fn get_iter_big() {
-        let mut list = LinkedList::new();
-        list.push(10);
-        list.push(20);
-        list.push(30);
-        list.push(100);
-        list.push(200);
-        list.push(300);
-        list.push(1000);
-        list.push(2000);
-        list.push(3000);
-
-        let actual = list.get_mut(5).map(|n| n.value);
-        assert_eq!(actual, Some(100));
+        assert_eq!(list.peek(), Some(&1));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.peek(), Some(&2));
     }
 
     #[test]
     fn from_vec() {
-        let vecy_poo = vec![1, 2, 3];
-        let actual_linky_poo = LinkedList::from(vecy_poo);
-        let mut expected_linky_poo = LinkedList::new();
+        let vec = vec![1, 2, 3];
+        let actual = LinkedList::from(vec);
+        let mut expected = LinkedList::new();
 
-        expected_linky_poo.push(3);
-        expected_linky_poo.push(2);
-        expected_linky_poo.push(1);
+        expected.push(1);
+        expected.push(2);
+        expected.push(3);
 
-        assert_eq!(actual_linky_poo, expected_linky_poo);
+        assert_eq!(actual.peek(), expected.peek());
+
+        // assert_eq!(actual_linky_poo, expected_linky_poo);
     }
 
     #[test]
     fn vec_macro() {
         let mut actual = ll![1, 2, 3];
         let mut expected = LinkedList::new();
-        expected.push(3);
-        expected.push(2);
         expected.push(1);
+        expected.push(2);
+        expected.push(3);
 
         assert_eq!(actual, expected);
     }
 
-    #[test]
-    fn clone() {
-        let original = ll![1, 2, 3];
-        let cloned = original.clone();
-
-        assert_eq!(original, cloned);
-    }
+    // #[test]
+    // fn clone() {
+    //     let original = ll![1, 2, 3];
+    //     let cloned = original.clone();
+    //
+    //     assert_eq!(original, cloned);
+    // }
 }
--- src/util/math_utils.rs
+++ src/util/math_utils.rs
@@ -1,32 +1,47 @@
 use factorial::factorial;
-use std::{collections::HashMap, io::Empty};
+use std::{collections::HashMap, io::Empty, ptr::{self, null_mut, null}};
 
 use crate::util::linked_list::Node;
 
 use super::linked_list::LinkedList;
 
-// [1, 1, 2, 4] multiset
-pub fn multiset_permute(set: Vec<usize>) {
-    // let permutations = vec![vec![0, set.len()], calculate_unique_permutations(set)];
-    // let mut permutations: Vec<Vec<usize>> = Vec::with_capacity(calculate_unique_permutations(&set));
-    // let mut slice = set.clone();
-    // slice.reverse();
-    // let mut ll = LinkedList::from(slice);
-    //
-    // type Link<'a> = Option<&'a mut Node<usize>>;
-    // let h: Link = ll.get_mut(0); // first element
-    // let i: Link = ll.get_mut(set.len() - 2); // second to last element
-    // let j: Link = ll.get_mut(set.len() - 1); // last element
-    // let s: Link = None;
-    // let t: Link = None;
-    //
-    // // loopless iteration
-    // // while let Some(jnext) = j.map(|n| n.next) {
-    // while (j.map(|n| n.next).is_some() || j.map(|n| n.value) < h.map(|n| n.value)) {
-    //     let mut single_perm = ll.clone();
-    //
-    //     permutations.push(Vec::from(single_perm));
-    // }
+use crate::ll;
+
+// Based off this algorithim: https://epubs.siam.org/doi/pdf/10.1137/1.9781611973068.107
+pub fn multiset_permute(set: Vec<usize>) -> Vec<Vec<usize>> {
+    let mut permutations: Vec<Vec<usize>> = Vec::with_capacity(calculate_unique_permutations(&set));
+    let mut slice = set.clone();
+    let mut ll = LinkedList::from(slice);
+
+    unsafe {    
+        let mut h = ll.get_raw_reference(0); // first element
+        let mut i = ll.get_raw_reference(set.len() - 2); // second to last element
+        let mut j = ll.get_raw_reference(set.len() - 1); // last element
+        let mut s: *mut Node<usize> = ptr::null_mut();
+        let mut t: *mut Node<usize> = ptr::null_mut();
+
+        permutations.push(Vec::from(&ll));
+        while (*j).next != null_mut() || (*j).value < (*h).value {
+            if (*j).next != null_mut() && (*i).value >= (*(*j).next).value {
+                s = j;
+            } else {
+                s = i;
+            }
+            t = (*s).next;
+            (*s).next = (*t).next;
+            (*t).next = h;
+            if ((*t).value < (*h).value) {
+                i = t;
+            }
+            j = (*i).next;
+            h = t;
+            ll.set_head(h);
+
+            permutations.push(Vec::from(&ll));
+        }
+    }
+
+    permutations
 }
 
 // n!/(m1! * m2! ... * mn!)
@@ -55,7 +70,7 @@ mod factorial {
     use once_cell::sync::Lazy;
     use std::collections::HashMap;
 
-    const FACTORIAL_TABLE: Lazy<HashMap<usize, usize>> = Lazy::new(|| HashMap::new());
+    static FACTORIAL_TABLE: Lazy<HashMap<usize, usize>> = Lazy::new(|| HashMap::new());
 
     pub fn factorial(num: usize) -> usize {
         if num < 2 {
@@ -72,6 +87,28 @@ mod factorial {
     }
 }
 
+#[test]
+fn test_multiset_permute() {
+    let vec = vec![4, 2, 1, 1];
+    let actual = multiset_permute(vec);
+    let expected = vec![
+        vec![4, 2, 1, 1], 
+        vec![1, 4, 2, 1], 
+        vec![4, 1, 2, 1],
+        vec![1, 4, 1, 2],
+        vec![1, 1, 4, 2],
+        vec![4, 1, 1, 2],
+        vec![2, 4, 1, 1],
+        vec![1, 2, 4, 1],
+        vec![2, 1, 4, 1],
+        vec![1, 2, 1, 4],
+        vec![1, 1, 2, 4],
+        vec![2, 1, 1, 4]
+    ];
+
+    assert_eq!(actual, expected);
+}
+
 // [1, 1, 2, 4] multiset
 // [(1, 2), (2, 1), (4, 1)]
 // n!/(m1! * m2! ... * mn!)
@@ -81,9 +118,9 @@ mod factorial {
 
 #[test]
 fn test_calc_uniq_perms() {
-    let multis = vec![1, 1, 2, 4];
+    let multis = vec![4, 2, 1, 1];
 
-    // assert_eq!(calculate_unique_permutations(multis), 12);
+    assert_eq!(calculate_unique_permutations(&multis), 12);
 }
 
 #[test]
