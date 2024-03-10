f5146ceada12b496c22b40715e0081e9f5e94643
Nick Spinosa <nickspinosa1022@gmail.com>
2024-03-10 00:11:22 -0500

    builds states and adds comments

--- src/calculator/calculator.rs
+++ src/calculator/calculator.rs
@@ -25,18 +25,25 @@ pub fn calculate_slot_transistion_matrix(
 
     let transition_matrix: DMatrix<u8> = DMatrix::zeros(matrix_size.into(), matrix_size.into());
 
-    let mut ranges: Vec<Vec<u8>> = target_champions
+    /// each sub vec represents the range of a given champion you can find from 0 to your desired amount
+    let ranges: Vec<Vec<u8>> = target_champions
         .iter()
         .map(|tc| tc.get_amount_needed())
         .map(|amount| {
             let mut vec = Vec::new();
-            for i in 0..amount {
+            for i in 0..=amount {
                 vec.push(i);
             }
             vec
         })
         .collect();
 
+    /// states is the cartesian product of the above ranges
+    /// it represents every permutation of outcomes possible
+    /// i.e. 2 kaisa, 0 sett
+    ///      1 kaisa, 1 sett
+    ///      0 kaisa, 2 set
+    ///      etc..
     let states: Vec<Vec<u8>> = ranges
         .into_iter()
         .map(|v| v.into_iter())
